f27827b036f19f25333c6565b0897872e87bcae0
Thomas777666 <thomaszakharchenko18@gmail.com>
2026-05-10 16:31:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-TOKEN = '5749426974:AAHEZsqDvmhB2fOCa5ZAV7lKSXJouOyFXNg'
+TOKEN = '8336283371:AAFFtBfCxTLLNJ87qE0xsz4WgF1VVLnKkQg'
 
 
 dp = Dispatcher()
